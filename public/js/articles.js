dba176ec3f02322dc9bdb6bba465ed107dbaaee9
unknown <gyenes.bela@szbiszeged.local>
2025-09-04 14:53:24 +0200

    feltoltve

---
+++ public/js/articles.js
@@ -0,0 +1,16 @@
+let articles = [
+    {
+        nev: "Minecraft porno",
+        description: "hmmmmmmmmmmmmmmmm"
+    },
+    {
+        nev: "Kakaman kalandjai a fanszőrzet bugyraiban",
+        description: "haha"
+    },
+    {
+        nev: "Kódolás",
+        description: "programozá"
+    }
+]
+
+module.exports = articles;
---
+++ public/js/subscribers.js
@@ -0,0 +1,16 @@
+let subscribers = [
+    {
+        nev: "Előfizető 1",
+        status: true
+    },
+    {
+        nev: "Előfizető 2",
+        status: true
+    },
+    {
+        nev: "Előfizető 3",
+        status: true
+    }
+]
+
+module.exports = subscribers;
